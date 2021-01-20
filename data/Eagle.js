31ca0e2f9fbdf6c359c5ed1e1f82fe24de69a4c3
dmenaker <58150573+dmenaker@users.noreply.github.com>
2021-01-19 22:11:26 -0500

    eagle update

--- data/Eagle.js
+++ data/Eagle.js
@@ -18,7 +18,7 @@ var eagleJson =
     "type":"Feature",
     "properties":{
         "Scout": "Anthony Plaag",
-        "Description": "Bird Thing",
+        "Description": "NEST Project",
         "Beneficiary": "",
         "Troop": "109"
     },
@@ -127,17 +127,6 @@ var eagleJson =
     {
     "type":"Feature",
     "properties":{
-        "Scout": "",
-        "Description": "Nature Trail",
-        "Beneficiary": "Plumsted Township",
-        "Troop": "109"
-    },
-    "geometry":{"type":"Point", "coordinates":[-74.479442, 40.069123]}
-    },
-
-    {
-    "type":"Feature",
-    "properties":{
         "Scout": "Randy Barnes",
         "Description": "Rebuild the Service Member Honor Roll Wall",
         "Beneficiary": "American Legion Post 455",
@@ -226,17 +215,243 @@ var eagleJson =
     {
     "type":"Feature",
     "properties":{
+        "Scout": "Christian Millward",
+        "Description": "Renovation of storage shed roof and shelving",
+        "Beneficiary": "Helping Hands Food Pantry",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.536725, 40.085221]}
+    },
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Justin Reuter",
+        "Description": "Custom bench, sidewalk restoration and fencing",
+        "Beneficiary": "Helping Hands Food Pantry",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.536725, 40.085221]}
+    },
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Cody Meyers",
+        "Description": "Fire Pit",
+        "Beneficiary": "New Egypt Church of Christ",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.514758, 40.072530]}
+    },
+
+    {
+    "type":"Feature",
+    "properties":{
         "Scout": "Dylan Manuguerra",
+        "Description": "Cat shelter",
+        "Beneficiary": "Calling All Cats",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.33726, 40.10889]}
+    },
+
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Dustin Mooney",
+        "Description": "Cat shelter",
+        "Beneficiary": "Calling All Cats",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.33761, 40.10889]}
+    },
+
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Alexander Parsells",
+        "Description": "Nature trail extension and bench",
+        "Beneficiary": "Plumsted Township",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.524865, 40.083238]}
+    },
+
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Nicholas Pawlyzyn",
+        "Description": "Renovated flagpole, installed a bench, pavers, and a plaque",
+        "Beneficiary": "New Hanover School District",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },
+
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "John Rinaldi",
+        "Description": "Installed 9/11 Memorial",
+        "Beneficiary": "Jensen's Deep Run",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Patrick McMahon",
+        "Description": "Renovated signage",
+        "Beneficiary": "Riding High Farm",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Clayton Ingalls",
+        "Description": "Installed main trail kiosk",
+        "Beneficiary": "Emson Preserve",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Michael Guliano",
+        "Description": "Installed sign at roadway",
+        "Beneficiary": "Emson Preserve",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Edward Reynolds",
+        "Description": "Installed informational sign and bench",
+        "Beneficiary": "Emson Preserve",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Ryan Twarkusky",
         "Description": "",
         "Beneficiary": "",
         "Troop": "109"
     },
-    "geometry":{"type":"Point", "coordinates":[-74.0, 40.0]}
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Corey Mount",
+        "Description": "Installed informational sign and bench",
+        "Beneficiary": "Emson Preserve",
+        "Troop": "109"
     },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Luke Slivenko",
+        "Description": "Installed informational kiosk about fish",
+        "Beneficiary": "Plumsted Township",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Christian Olivo",
+        "Description": "Bluebird habitat trail, installed bench and sign",
+        "Beneficiary": "Emson Preserve",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Jamie Enriquez",
+        "Description": "Installed informational sign and bench",
+        "Beneficiary": "Emson Preserve",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Alejandro Lizano",
+        "Description": "Built food pantry",
+        "Beneficiary": "Saint Monica's Church",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Tanyon Olivo",
+        "Description": "Elizabeth Meirs Morgan Wildflower & Butterfly Garden",
+        "Beneficiary": "Emson Preserve",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.61877, 40.03529]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "William Palzer",
+        "Description": "Relined driveway and installed pavers",
+        "Beneficiary": "Immanuel Luthern Church",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.22957, 40.08878]}
+    },     
+      
+    {
+    "type":"Feature",
+    "properties":{
+        "Scout": "Daniel Palzer",
+        "Description": "Nursery school renovation",
+        "Beneficiary": "Immanuel Luthern Church",
+        "Troop": "109"
+    },
+    "geometry":{"type":"Point", "coordinates":[-74.22978, 40.08905]}
+    },     
+      
+      
+    
+      
+      
+      
 
-
-
-
+      
+     
+   
+      
 
     {
     "type":"Feature",
